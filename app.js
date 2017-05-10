70dc7e5a4d52609f92f4672a542f08d7b0ba87d0
romainberthome <romainberthome@gmail.com>
2017-05-10 20:50:20 +0000

    index and new form for sport and new modules

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require("express"),
     app     = express(),
     mongoose = require("mongoose"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    methodOverride = require("method-override");
     
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,6 +17,25 @@ app.get("/", function(req, res){
 app.get("/home", function(req, res){
     res.render("home");
 });
+
+// Sport routes
+
+app.get("/sports", function(req, res) {
+    res.render("sports/index");
+});
+
+app.get("/sports/new", function(req, res) {
+    res.render("sports/new");
+});
+
+app.get("/sports/:id", function(req, res) {
+    res.render("sports/show");
+});
+
+app.get("sport/:id/edit", function(req, res) {
+    res.render("sport/edit");
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server Started");
